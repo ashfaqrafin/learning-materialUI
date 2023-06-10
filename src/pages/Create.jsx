4f3ccd85bab84353e3f3a36a1ad76790f202c1a0
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-06-10 18:28:56 +0600

    Tutorial3

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,22 @@
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 const Create = () => {
-  return <div className="create">Create Page</div>;
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Blog
+      </Typography>
+      <Button type="submit" color="secondary" variant="contained">
+        Submit
+      </Button>
+    </Container>
+  );
 };
 
 export default Create;
